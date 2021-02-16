b09ec745b0db483b9a639ff3efcbd2b3f413fc54
TaeHo Kim <kimtaeho001202@gmail.com>
2021-02-16 18:58:03 +0900

    environment renewal

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # Surround Client
-
-이 프로젝트는 [Create React App](https://github.com/facebook/create-react-app). 을 기반으로 만들어졌습니다.
-
-### `yarn start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
 import React from 'react';
-import GlobalContainer from 'react-div-100vh';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from 'react-router-dom';
 
-import Header from './common/Header';
-import Home from './Home';
-import Room from './Room';
+import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <GlobalContainer>
-
-      <Header />
-
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={Home}
-          />
-          <Route
-            exact
-            path="/room"
-            component={Room}
-          />
-        </Switch>
-      </BrowserRouter>
-
-    </GlobalContainer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
